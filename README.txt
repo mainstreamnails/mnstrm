323859f273d1ae6a166bb4cf49da5abb5f59b1bb
Ena Vu <Ena.Vu@flyfrontier.com>
2017-10-20 10:56:42 -0600

    first init

---
+++ README.txt
@@ -0,0 +1,1 @@
+mainstreamnails.com by enavu
---
+++ js/index.js
@@ -0,0 +1,14 @@
+$(window).on('load', function() {
+    $("body").removeClass("preload");
+  });
+  $(window).on('load',function() { 
+    $("#loading").fadeOut("fast"); 
+});
+  $(document).ready(function() {
+    $('#hideMe').fadeOut(8000);// 5 seconds x 1000 milisec = 5000 milisec
+    $('#close').fadeIn(10000);
+    $('#text').fadeIn(10000);
+
+
+
+});
