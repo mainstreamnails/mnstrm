2a0120c7beadb72403f86a25c0b4b858a0aca218
enavu <enavuio@MacBooks-MacBook-Pro.local>
2017-10-22 12:35:09 -0600

    fixed animation logo

--- js/index.js
+++ js/index.js
@@ -1,14 +1,16 @@
 $(window).on('load', function() {
     $("body").removeClass("preload");
-  });
-  $(window).on('load',function() { 
-    $("#loading").fadeOut("fast"); 
 });
-  $(document).ready(function() {
-    $('#hideMe').fadeOut(8000);// 5 seconds x 1000 milisec = 5000 milisec
-    $('#close').fadeIn(10000);
+$(window).on('load', function() {
+    $("#loading").fadeOut("fast");
+});
+$(document).ready(function() {
+    $('#hideMe').fadeOut(8000); // 5 seconds x 1000 milisec = 5000 milisec
     $('#text').fadeIn(10000);
-
-
-
 });
+
+setTimeout(resetCSS, 8000);
+
+function resetCSS() {
+    $('#close').fadeIn("slow");
+}
